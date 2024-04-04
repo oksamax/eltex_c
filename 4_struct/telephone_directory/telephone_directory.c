11ef2ead2e94305456448e09fd8999d0643142d5
home_pc <maxim>
2024-04-04 22:55:28 +0700

    telephone directory

---
+++ 4_struct/telephone_directory/telephone_directory.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <string.h>
+
+struct list {
+    char first_name[16];
+    char second_name[16];
+    char number[16];
+};
+
+int main(void)
+{
+    struct list user[10];
+    int item = 0, counter = 0, i = 0, counter_fd = 0;
+    char find_delete[16] = {'0'};
+
+    do {
+        puts("***Telephone directory***\n     Menu:\n   1.Add user\n   2.List of users\n   3.Find user\n   4.Delete user\n   5.Exit\n\n");
+        
+        printf("Enter the item:");
+        scanf("%d", &item);
+
+        if (1 == item) {
+            if (counter < 10) {
+                printf("Enter the first name:");
+                scanf("%s", user[counter].first_name);
+                printf("Enter the second name:");
+                scanf("%s", user[counter].second_name);
+                printf("Enter the number:");
+                scanf("%s", user[counter].number);
+                counter++;
+            }
+            else puts("The telephone directory is full!!!");
+        }
+        else if (2 == item) {
+            if (counter > 0) {
+                for(i = 0; i < counter; i++) {
+                    printf("%d. First name: %-16s  Second name: %-16s  Number: %-16s\n",i+1, user[i].first_name, user[i].second_name, user[i].number);
+                }
+            }
+            else puts("The telephone directory is empty!");
+
+        }
+        else if (3 == item) {
+            if (counter > 0) {
+                printf("Enter a second name to search:");
+                scanf("%s", find_delete);
+                for(i = 0; i < counter; i++) {
+                    if (!(strcmp(find_delete, user[i].second_name))) {
+                        printf("%d. First name: %-16s  Second name: %-16s  Number: %-16s\n",counter_fd+1, user[i].first_name, user[i].second_name, user[i].number);
+                        counter_fd++;
+                    }
+                }
+                if(0 == counter_fd) puts("This user is not in the telephone directory!!!");
+                counter_fd = 0;
+            }
+            else puts("The telephone directory is empty!");
+        }
+        else if (4 == item) {
+            if (counter > 0) {
+                printf("Enter a first name to delete:");
+                scanf("%s", find_delete);
+                for(i = 0; i < counter; i++) {
+                    if (!(strcmp(find_delete, user[i].second_name))) {
+                        if (counter == 1) {
+                            printf("Delete user: First name: %-16s  Second name: %-16s  Number: %-16s\n", user[i].first_name, user[i].second_name, user[i].number);
+                            counter--;
+                            counter_fd++;
+                        }
+                        else {
+                            printf("Delete user: First name: %-16s  Second name: %-16s  Number: %-16s\n", user[i].first_name, user[i].second_name, user[i].number);
+                            strcpy(user[counter-1].first_name, user[i].first_name);
+                            strcpy(user[counter-1].second_name, user[i].second_name);
+                            strcpy(user[counter-1].number, user[i].number);
+                            counter--;
+                            i--;
+                            counter_fd++;
+                        }
+                    }
+                }
+                if(0 == counter_fd) puts("This user is not in the telephone directory!!!");
+                counter_fd = 0;
+            }
+            else puts("The telephone directory is empty!");
+        }
+        else if (item != 5){
+            puts("This item is not in the menu!!!");
+        }
+        puts("\n\n");
+    } while(item != 5);
+
+
+
+    return 0;
+}
