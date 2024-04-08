f3d28b78fea1519502af1cb16ee77d7b0f06dae3
home_pc <maxim>
2024-04-08 22:35:24 +0700

    7

--- 7_static_library/calculator.c
+++ 7_static_library/calculator.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    int var_a = 0, var_b = 0, item = 0;
+    int var_a = 0, var_b = 0, item = 0, result = 0;
 
     do {
         printf("***Calculator***\n     Menu:\n   1.Additional\n   2.Subtraction\n   3.Multiplication\n   4.Division\n   5.Exit\n\n");
@@ -14,28 +14,32 @@ int main(void)
             scanf("%d", &var_a);
             printf("Enter variable b:");
             scanf("%d", &var_b);
-            printf("Result (a + b) = %d\n", add(var_a, var_b));
+            result = add(var_a, var_b);
+            printf("Result (a + b) = %d\n", result);
         }
         else if (2 == item) {
             printf("Subtraction (a - b)\nEnter variable a:");
             scanf("%d", &var_a);
             printf("Enter variable b:");
             scanf("%d", &var_b);
-            printf("Result (a - b) = %d\n", sub(var_a, var_b));
+            result = sub(var_a, var_b);
+            printf("Result (a - b) = %d\n", result);
         }
         else if (3 == item) {
             printf("Multiplication (a * b)\nEnter variable a:");
             scanf("%d", &var_a);
             printf("Enter variable b:");
             scanf("%d", &var_b);
-            printf("Result (a * b) = %d\n", mul(var_a, var_b));
+            result = mul(var_a, var_b);
+            printf("Result (a * b) = %d\n", result);
         }
         else if (4 == item) {
             printf("Division (a \\ b)\nEnter variable a:");
             scanf("%d", &var_a);
             printf("Enter variable b:");
             scanf("%d", &var_b);
-            printf("Result (a \\ b) = %d\n", div(var_a, var_b));
+            result = div(var_a, var_b);
+            printf("Result (a \\ b) = %d\n", result);
         }
         else if (item != 5) printf("This item is not in the menu!!!");
         printf("\n\n");
